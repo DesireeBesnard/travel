481ae919517ca0a99df3ba6b1b7a60b697a319cd
DesireeBesnard <desiree.besnard@gmail.com>
2021-02-01 20:33:14 +0100

    Nav bar structure created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Router path="/" exact />
+        </Switch>
+      </Router>
+
+    </>
+    
   );
 }
 
