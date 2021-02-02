ff64f67ee3b4249f0146f6c8bede5d1495405fd3
DesireeBesnard <desiree.besnard@gmail.com>
2021-02-02 11:25:52 +0100

    responsive navbar done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// React-router-dom comprend l'API de routage requise pour les navigateurs
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
